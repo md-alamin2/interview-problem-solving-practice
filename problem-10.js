fc9d6c7b3a6e3b3a35e256e8e1881c3a5ba77f74
md-alamin2 <mdalamin22671@gmail.com>
2025-08-26 15:22:24 +0600

    solved problem no. 10

--- problem-10.js
+++ problem-10.js
@@ -1,19 +1,21 @@
 // Find the most frequent character in a string
 //  Input: "javascript" → Output: "a"
-// TODO: I WILL DO IT LATER
+
 const frequentCharacter = (str) => {
   const newStr = str.toLowerCase();
-  console.log(newStr);
-  let compare = newStr[0];
-  let finalChar
+ const strCount = {}
+ let mostFrequentLength = 0;
+ let mostFrequent = ""
   for (let char of newStr) {
-    if (compare === char) {
-      finalChar = char;
-    } else {
-      compare = char;
+    strCount[char] = (strCount[char] || 0) + 1;
+  }
+  for (let char of newStr){
+    if(strCount[char] > mostFrequentLength){
+      mostFrequentLength = strCount[char]
+      mostFrequent = char
     }
   }
-  console.log(finalChar)
+  console.log(mostFrequent)
 };
 
-frequentCharacter("Programming hero");
+frequentCharacter("Javascript")
